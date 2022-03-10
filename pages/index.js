ca8cc27fc85612471597b3372828cb365e57f9b8
Fede <f.gomezlara@outlook.es>
2022-03-10 15:22:15 +0100

    fetch pokemon from the api

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import ContainerBlock from '../components/ContainerBlock'
 
-const Home = () => {
+const Home = ({ pokemon }) => {
+  console.log(pokemon)
   return (
     <ContainerBlock title="Next.js Pokédex">
       <h1 className="text-4xl mb-8 text-center">Next.js Pokédex</h1>
@@ -9,3 +10,30 @@ const Home = () => {
 }
 
 export default Home
+
+// By defining getStaticProps, Next knows its a static page. At build time it will
+// run this function, grab the data from the Pokémon API and then pass data to the
+// index page
+export const getStaticProps = async () => {
+  try {
+    // Fetch 135 Pokémon starting by 251 (251 is excluded)
+    const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=135&offset=251')
+    // Destructure results directly from the response
+    const { results } = await res.json()
+
+    const pokemon = results.map((result, index) => {
+      return {
+        ...result,
+        // This will return 252 for the first Pokémon which is number 252 and so on
+        image: `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${(index + 252).toString()}.png`
+      }
+    })
+
+    return {
+      props: { pokemon }
+    }
+  }
+  catch (err) {
+    console.log('Error fetching Pokémon from the API:', err)
+  }
+}
